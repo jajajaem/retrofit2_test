9000690c16330473d1907340ccca3d9a87b80a79
jajajaem <woals7301@naver.com>
2025-03-03 02:13:30 +0900

    retrofit2_test

--- app/src/main/java/com/test/retrofit_test/MainActivity.kt
+++ app/src/main/java/com/test/retrofit_test/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.test.retrofit_test
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,8 +14,9 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var tv1: TextView
+    private lateinit var tv1: EditText
     private lateinit var tv2: TextView
+    private lateinit var btn1: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,22 +24,30 @@ class MainActivity : AppCompatActivity() {
 
         tv1 = findViewById(R.id.tv1)
         tv2 = findViewById(R.id.tv2)
-        val retrofitInstance = RetrofitInstance.getInstance().create(ApiInterface::class.java)
-        retrofitInstance.getUserData(tv1.text.toString()).enqueue(object : Callback<userInfo>{
-            override fun onResponse(p0: Call<userInfo>, p1: Response<userInfo>) {
-                val result = p1.body()
-                tv2.text =result.toString()
-            }
+        btn1 = findViewById(R.id.btn_1)
 
-            override fun onFailure(p0: Call<userInfo>, p1: Throwable) {
-            }
-        })
+        aa()
 
+        btn1.setOnClickListener {
+            aa()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    private fun aa(){
+        val retrofitInstance = RetrofitInstance.getInstance().create(ApiInterface::class.java)
+        retrofitInstance.getUserData(tv1.text.toString()).enqueue(object : Callback<userInfo> {
+            override fun onResponse(p0: Call<userInfo>, p1: Response<userInfo>) {
+                val result = p1.body()
+                tv2.text = result.toString()
+            }
 
+            override fun onFailure(p0: Call<userInfo>, p1: Throwable) {
+                tv2.text = "실패했습니다."
+            }
+        })
     }
 }
