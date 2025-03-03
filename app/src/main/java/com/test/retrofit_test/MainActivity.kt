da2804d27be70e565ec7144537b35d02772b139b
jajajaem <woals7301@naver.com>
2025-03-04 00:49:46 +0900

    retrofit2_test(get, post) , recyclerview

--- app/src/main/java/com/test/retrofit_test/MainActivity.kt
+++ app/src/main/java/com/test/retrofit_test/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.test.retrofit_test
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -17,15 +19,18 @@ import com.test.retrofit_test.retrofit2.userInfoItem
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
     private lateinit var tv1: EditText
     private lateinit var tv2: TextView
     private lateinit var btn1: Button
+    private lateinit var btn2: Button
 
     //val listItem = arrayListOf<listlayout>()
     private val userList = arrayListOf<userInfoItem>()
     val customAdapter = CustomAdapter(userList)
+    var input = HashMap<String, Any>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,16 +41,22 @@ class MainActivity : AppCompatActivity() {
         tv1 = findViewById(R.id.tv1)
         tv2 = findViewById(R.id.tv2)
         btn1 = findViewById(R.id.btn_1)
+        btn2 = findViewById(R.id.btn_2)
+
+        input["id"] = "11"
+        input["phone"] = "123123123"
+        input["emain"] = "woaks@kajc.ci"
 
 
         val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = customAdapter
 
-        aa()
-
         btn1.setOnClickListener {
-            aa()
+            retrofit_get()
+        }
+        btn2.setOnClickListener {
+            retrofit_post()
         }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -55,7 +66,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     //레트로핏 get
-    private fun aa() {
+    private fun retrofit_get() {
         val retrofitInstance = RetrofitInstance.getInstance().create(ApiInterface::class.java)
         retrofitInstance.getUserData(Integer.parseInt(tv1.text.toString())).enqueue(object : Callback<List<userInfoItem>> {
             override fun onResponse(
@@ -72,6 +83,22 @@ class MainActivity : AppCompatActivity() {
 
             override fun onFailure(p0: Call<List<userInfoItem>>, p1: Throwable) {
                 tv2.text = "실패했습니다."
+            }
+        })
+    }
+
+    private fun retrofit_post(){
+        val retrofitInstance = RetrofitInstance.getInstance().create(ApiInterface::class.java)
+        retrofitInstance.getPostList(input).enqueue(object : Callback<userInfoItem>{
+            override fun onResponse(p0: Call<userInfoItem>, p1: Response<userInfoItem>) {
+                Log.d(TAG, "post ok")
+                Log.d(TAG, "${p1.body()}")
+                tv2.text="post 성공"
+
+            }
+
+            override fun onFailure(p0: Call<userInfoItem>, p1: Throwable) {
+                tv2.text="post 실패"
             }
         })
     }
--- app/src/main/java/com/test/retrofit_test/retrofit2/ApiInterface.kt
+++ app/src/main/java/com/test/retrofit_test/retrofit2/ApiInterface.kt
@@ -3,10 +3,19 @@ package com.test.retrofit_test.retrofit2
 import retrofit2.http.GET
 import retrofit2.http.Query
 import retrofit2.Call
+import retrofit2.http.FieldMap
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
 
 interface ApiInterface {
     @GET("users/")
     fun getUserData(
         @Query("page")page : Int
     ): Call<List<userInfoItem>>
+
+    @FormUrlEncoded
+    @POST("posts/")
+    fun getPostList(
+        @FieldMap param: HashMap<String, Any>
+    ): Call<userInfoItem>
 }
